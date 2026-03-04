ae8831c27cb946c3c889af46985a2a9b2751fef4
Nguyen Huan <youremail@example.com>
2026-03-04 21:24:19 +0700

    Update V26.03.04.21.24

--- modules/product.js
+++ modules/product.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const connectSchema = require("../Schema/product");
 const mongoose = require('mongoose');
-
 const CheckToken = require("../modules/middlewares/checkToken");
 const auth = new CheckToken();
 const connectSchema__User = require("../Schema/user");
@@ -21,6 +20,32 @@ const addProductLogic = async (req, res, next) => {
         if (error) return next(error);
     }
 };
+
+
+const Admin_EditProduct = async (req, res, next) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({ mesage_vn: 'Lỗi truy vấn', mesage_en: 'Erro query', Status: false });
+
+    try {
+        const { name, price, describe, remainingQuantity, present, priceSale } = req.body;
+        const dataEdit = { name, price, describe, remainingQuantity, present, priceSale }
+        const result = await connectSchema.findByIdAndUpdate(
+            id,
+            { $set: dataEdit },
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!result) return res.status(400).json({ mesage_vn: 'Cập nhật thất bại', mesage_en: 'Update failed', status: false });
+        return res.status(200).json({ mesage_vn: 'Cập nhật thành công', mesage_en: 'Update successful', status: true });
+
+    } catch (error) {
+        if (error) return next(error);
+    }
+};
+
 
 
 // Danh sách sản phẩm điện thoại
@@ -141,26 +166,6 @@ const advertisement = async (req, res, next) => {
     }
 }
 
-
-const transitionAdvertisement = async (req, res, next) => {
-    const _id = req.params.id;
-    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ mesage_vn: 'Lỗi truy vấn', mesage_en: 'Erro query', Status: false });
-    const { status } = req.query;
-
-    try {
-        const result = await connectSchema
-            .findByIdAndUpdate(
-                _id,
-                { $set: { advertisement: status } },
-                { new: true, runValidators: false }
-            );
-
-        if (!result) return res.status(400).json({ mesage_vn: 'Chuyển đổi trạng thái thất bại', mesage_en: 'State transition failed', status: false });
-        return res.status(200).json({ mesage_vn: 'Chuyển đổi trạng thái thành công', mesage_en: 'State transition successful', status: true });
-    } catch (error) {
-        if (error) return next(error);
-    }
-}
 
 
 const TrademarkPrduct = async (req, res, next) => {
@@ -314,11 +319,76 @@ const Admin_SelectProductSale = async (req, res, next) => {
     }
 }
 
+const Admin_SelectProductAdvertisement = async (req, res, next) => {
+    const { statusAdvertisement } = req.query;
+    const idUser = req.params.idUser;
+    if (!mongoose.Types.ObjectId.isValid(idUser)) return res.status(404).json({ error: 'Invalid _id', Status: false, data: [] });
+    const isAdvertisement = statusAdvertisement === 'true' ? true : false;
+
+
+    // Check Token
+    const token = req.headers.authorization?.split(' ')[1];
+    const decoded = auth.verifyAccessToken(token);
+    if (idUser !== decoded._id) return res.status(401).json({ message_en: 'You do not have access.', message_vn: 'Bạn không có quyền truy cập', status: false, data: [] });
+    const checkRole = await connectSchema__User.findOne({ _id: decoded._id }).select('role');
+    if (!checkRole || checkRole.role !== 'Admin') return res.status(401).json({ message_en: 'You do not have access.', message_vn: 'Bạn không có quyền truy cập', status: false, data: [] });
+
+
+    try {
+        const result = await connectSchema
+            .find({
+                status: true,
+                advertisement: isAdvertisement  // Sửa lỗi cú pháp so sánh ở đây
+            })
+            .select('name price img priceSale remainingQuantity')
+            .limit(10);
+
+        if (result.length === 0) return res.status(201).json({ mesage_vn: 'Không tìm thấy dữ liệu', mesage_en: 'Query failed', data: [], status: false });
+        return res.status(200).json({ mesage_vn: 'Truy vấn thành công nhé bạn', mesage_en: 'Query successful', data: result, status: false });
+    } catch (error) {
+        if (error) return next(error);
+    }
+}
+
+
+
+const Admin_transitionAdvertisement = async (req, res, next) => {
+    const _id = req.params.id;
+    const idUser = req.params.idUser;
+    if (!mongoose.Types.ObjectId.isValid(_id || idUser)) return res.status(404).json({ mesage_vn: 'Lỗi truy vấn', mesage_en: 'Erro query', Status: false });
+    const { status } = req.query;
+
+    // Check Token
+    const token = req.headers.authorization?.split(' ')[1];
+    const decoded = auth.verifyAccessToken(token);
+    if (idUser !== decoded._id) return res.status(401).json({ message_en: 'You do not have access.', message_vn: 'Bạn không có quyền truy cập', status: false, data: [] });
+    const checkRole = await connectSchema__User.findOne({ _id: decoded._id }).select('role');
+    if (!checkRole || checkRole.role !== 'Admin') return res.status(401).json({ message_en: 'You do not have access.', message_vn: 'Bạn không có quyền truy cập', status: false, data: [] });
+
+
+    try {
+        const result = await connectSchema
+            .findByIdAndUpdate(
+                _id,
+                { $set: { advertisement: status } },
+                { new: true, runValidators: false }
+            );
+
+        if (!result) return res.status(400).json({ mesage_vn: 'Chuyển đổi trạng thái thất bại', mesage_en: 'State transition failed', status: false });
+        return res.status(200).json({ mesage_vn: 'Chuyển đổi trạng thái thành công', mesage_en: 'State transition successful', status: true });
+    } catch (error) {
+        if (error) return next(error);
+    }
+}
+
+
 
 
 
 // Cline
-router.post("/add", addProductLogic);
+
+
+
 router.get("/view-product-phone", getProduct_Phone);
 router.get("/view-product-accessory", getProduct_Accessory);
 router.get("/view-Trademark-Product/:id", TrademarkPrduct);
@@ -333,13 +403,20 @@ router.get("/view-advertisement", advertisement);
 
 
 // Admin
+router.post("/add", addProductLogic);
+router.put("/admin-Product-Edit/:id", Admin_EditProduct);
+
 router.get("/admin-SelectAll/:idUser", Admin_SelectProduct); // tất cả sản phẩm 
 router.get("/admin-Select-ProductSale&No-Sale/:idUser", Admin_SelectProductSale); // DSach SP SALE và Không Sale ĐK SALE & NO
 // router.get("/admin-Number-Of-Product-Sales/:idUser", Admin_NumberOfProductSales)
 router.get("/admin-Detail/:id/:idUser", Admin__DetailProduct); // chi tiết sản phẩm
+router.get("/admin-Select-ProductSale&No-Sale/:idUser", Admin_SelectProductSale); // DSach SP SALE và Không Sale ĐK SALE & NO
+
+// Quảng cáo
+router.get("/admin-Select-ProductAdvertisement/:idUser", Admin_SelectProductAdvertisement);
+router.put("/admin-state-Transition-ProductAdvertisement/:id/:idUser", Admin_transitionAdvertisement);
 
 
 
 router.put("/state-Transition/:id", stateTransition);
-router.put("/state-Transition-advertisement/:id", transitionAdvertisement);
 module.exports = router;

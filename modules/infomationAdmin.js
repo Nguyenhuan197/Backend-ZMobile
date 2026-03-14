82668df7cc629f4f7f423ce70b7a1df1ae1a4046
Nguyen Huan <youremail@example.com>
2026-03-14 15:44:42 +0700

    Update V26.03.15.15.44

--- modules/infomationAdmin.js
+++ modules/infomationAdmin.js
@@ -55,13 +55,16 @@ const viewAdmin = async (req, res, next) => {
     }
 }
 
+
 const update = async (req, res, next) => {
     const _id = req.params.id;
     const _idUser = req.params.idUser;
-
     if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ mesage_vn: 'Lỗi truy vấn', mesage_en: 'Erro query', Status: false });
-    const { nameLogo, contact, phone, nameAdmin, address, email, slogan, pageFB, ticktock, shopee, workingHours } = req.body;
-    const dataUpdate = { nameLogo, contact, phone, nameAdmin, address, email, slogan, pageFB, ticktock, shopee, workingHours }
+    const {
+        nameLogo, contact, phone, nameAdmin, address, email, slogan, pageFB, ticktock, shopee, workingHours, partnerDelivery
+    } = req.body;
+    const dataUpdate = { nameLogo, contact, phone, nameAdmin, address, email, slogan, pageFB, ticktock, shopee, workingHours, partnerDelivery }
+
 
 
     // Check Token
@@ -79,12 +82,44 @@ const update = async (req, res, next) => {
                 { new: true, runValidators: false }
             );
 
-        if (!result) return res.status(400).json({ mesage_vn: 'Cập nhật thất bại =', mesage_en: 'Update failed', status: false });
+        if (!result) return res.status(400).json({ mesage_vn: 'Cập nhật thất bại', mesage_en: 'Update failed', status: false });
         return res.status(200).json({ mesage_vn: 'Cập nhật thành công', mesage_en: 'Update successful', status: true });
     } catch (error) {
         if (error) return next(error);
     }
 }
+
+
+const stateTransition = async (req, res, next) => {
+    const _id = req.params.id;
+    const _idUser = req.params.idUser;
+    const { status } = req.query;
+    if (!mongoose.Types.ObjectId.isValid(_id || _idUser)) return res.status(404).json({ mesage_vn: 'Lỗi truy vấn', mesage_en: 'Erro query', Status: false });
+
+
+    // Check Token
+    const token = req.headers.authorization?.split(' ')[1];
+    const decoded = auth.verifyAccessToken(token);
+    if (_idUser !== decoded._id) return res.status(401).json({ message_en: 'You do not have access.', message_vn: 'Bạn không có quyền truy cập', status: false, data: [] });
+    const checkRole = await connectSchema__User.findOne({ _id: decoded._id }).select('role');
+    if (!checkRole || checkRole.role !== 'Admin') return res.status(401).json({ message_en: 'You do not have access.', message_vn: 'Bạn không có quyền truy cập', status: false, data: [] });
+
+
+    try {
+        const result = await connectSchema
+            .findByIdAndUpdate(
+                _id,
+                { $set: { accountStatus: status } },
+                { new: true, runValidators: false }
+            );
+
+        if (!result) return res.status(400).json({ mesage_vn: 'Cập nhật thất bại', mesage_en: 'Update failed', status: false });
+        return res.status(200).json({ mesage_vn: 'Cập nhật thành công', mesage_en: 'Update successful', status: true });
+    } catch (error) {
+        if (error) return next(error);
+    }
+}
+
 
 
 router.post("/add", addNew);
@@ -93,4 +128,5 @@ router.get("/view", viewAll);
 // admin
 router.get("/admin-view/:idUser", viewAdmin);
 router.put("/update/:id/:idUser", update);
+router.put("/handle-state-transition/:id/:idUser", stateTransition);
 module.exports = router;
